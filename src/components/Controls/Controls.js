2e1f44e08620182651312ef1263034df7512f195
IvanKorneev <ivan.kornieiev@gmail.com>
2022-07-06 12:49:01 +0300

    add component Controls

---
+++ src/components/Controls/Controls.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+const optionsMap = {
+    'Africa': { value: 'Africa', label: 'Africa' },
+    'America': { value: 'America', label: 'America' },
+    'Asia': { value: 'Asia', label: 'Asia' },
+    'Europe': { value: 'Europe', label: 'Europe' },
+    'Oceania': { value: 'Oceania', label: 'Oceania' },
+}
+const options = Object.values(optionsMap);
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  @media (min-width: 767px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const Controls = () =>{
+    return(
+        <Wrapper>
+            <Search />
+            <CustomSelect
+                options={options}
+                placeholder="Filter by Region"
+                isClearable
+                isSearchable={false}
+                value={''}
+                onChange={() => {}}
+            />
+        </Wrapper>
+    );
+};
+export default Controls;
