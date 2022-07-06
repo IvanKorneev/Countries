e5a78b58b3b04cf7d07618d9ec89a9ad2603e98a
IvanKorneev <ivan.kornieiev@gmail.com>
2022-07-06 14:46:20 +0300

    refactor component App and add Route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,27 @@
 import Header from "../Header";
 import {Main} from "../Main/Main";
+import {Route, Routes} from "react-router-dom";
+import NotFound from "../NotFound";
+import Details from "../Details";
+import HomePage from "../HomePage";
 
 
 
 const App = () => {
     return (
         <>
-            <Header/>
-            <Main>
-                <HomePage/>
-            </Main>
+            <>
+                <Header />
+                <Main>
+                    <Routes>
+                        <Route exact path="/" element={
+                            <HomePage />
+                        } />
+                        <Route path="/country/:name" element={<Details />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Main>
+            </>
         </>
     )
 }
