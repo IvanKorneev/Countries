01de730a416b0db8d7cf4139a7d0065ff2e688e0
IvanKorneev <ivan.kornieiev@gmail.com>
2022-07-06 14:43:03 +0300

    add component HomePage

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,46 @@
+import Controls from "../Controls";
+import List from "../List";
+import {useNavigate} from "react-router-dom";
+import Card from "../Card";
+
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    const countries = [];
+    return (
+        <>
+            <Controls/>
+            <List>
+                {countries.map((c) => {
+                    const countryInfo = {
+                        img: c.flags.png,
+                        name: c.name,
+                        info: [
+                            {
+                                title: 'Population',
+                                description: c.population.toLocaleString(),
+                            },
+                            {
+                                title: 'Region',
+                                description: c.region,
+                            },
+                            {
+                                title: 'Capital',
+                                description: c.capital,
+                            },
+                        ],
+                    };
+
+                    return (
+                        <Card
+                            key={c.name}
+                            onClick={() => navigate(`/country/${c.name}`)}
+                            {...countryInfo}
+                        />
+                    );
+                })}
+            </List>
+        </>
+    );
+};
+export default HomePage;
