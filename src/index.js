15d92d6f35af1ac98fb5313fe65d8a953229ff27
IvanKorneev <ivan.kornieiev@gmail.com>
2022-07-06 12:22:28 +0300

    Initialize progect

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import '../../../../Downloads/code/07 - Проект 2/001 proj2-starter/src/index.css';
+import App from '../../../../Downloads/code/07 - Проект 2/001 proj2-starter/src/App';
+
+
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
