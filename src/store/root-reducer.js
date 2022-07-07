b7f222e63cbc0e9b7a47008a78616ccc59ed72fc
IvanKorneev <ivan.kornieiev@gmail.com>
2022-07-07 14:11:57 +0300

    create them-reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,4 +1,6 @@
 import {combineReducers} from "redux";
-export  const rootReducer = combineReducers({
+import {themeReducer} from "./theme/theme-reducer";
 
+export const rootReducer = combineReducers({
+    theme: themeReducer,
 });
---
+++ src/store/theme/theme-reducer.js
@@ -0,0 +1,11 @@
+import {SET_THEME} from "./theme-actions";
+
+const initialState = 'light';
+export const themeReducer = (state = initialState, {type, payload}) => {
+    switch (type) {
+        case SET_THEME:
+            return payload
+        default:
+            return state
+    }
+};
